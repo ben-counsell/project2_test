64848c4eb6451ea9cc78d08d770bb41cdf67006b
Ben Counsell <bencounsell@hotmail.co.uk>
2023-10-23 11:05:45 +0100

    tinkering

--- client/src/components/RecipeCard.jsx
+++ client/src/components/RecipeCard.jsx
@@ -1,7 +1,7 @@
 const RecipeCard = ({recipes}) => {
     
     const recipeGrid = recipes.map((recipe) => {
-        return <li key={recipe.key}>{recipe.title}<br/><img src={recipe.image}/></li>
+        return <li key={recipe.id}>{recipe.title}<br/><img src={recipe.image}/></li>
     })
     
     return (
