4f3b5d4c240f75f04c03db72ba237e624f20ea1e
Neil Roberts <neilroberts123@hotmail.co.uk>
2023-10-23 15:28:33 +0100

    still attempting recipe page

--- client/src/components/Thai.jsx
+++ client/src/components/Thai.jsx
@@ -1,6 +1,7 @@
 import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/splide/dist/css/themes/splide-default.min.css';
 
+
 const Thai = ({ thaiRecipes }) => {
   const thaiCard = thaiRecipes.map((recipe) => (
     <SplideSlide key={recipe.id}>
@@ -15,7 +16,9 @@ const Thai = ({ thaiRecipes }) => {
     <div>
       <h3>Thai Options</h3>
       <Splide options={{ type: 'loop', perPage: 4, pagination: false, drag: 'free', gap: '5rem' }}>
+        
         {thaiCard}
+        
       </Splide>
     </div>
   );
