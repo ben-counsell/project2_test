220b96802729bd14c32473ddfa73b93f44c87802
Joseph Aluko <dapoaluko18@gmail.com>
2023-10-23 15:21:56 +0100

    changesToDbAndNewFavouritesComponentAdded

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+// MongoClient require code
+const cors = require("cors");
+// create router require code
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
